47fa7a022675c2c7befbc9ee16740aebd2f7fee6
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-01-30 09:12:29 +0530

    feat: blog

---
+++ src/lib/components/Blog.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import BlogCard from '$lib/components/BlogCard.svelte';
+	import blogPosts from '$lib/data/blogs.js';
+	import BlurFade from './ui/BlurFade.svelte';
+
+	
+</script>
+
+<section class="bg-transparent py-20 pt-32">
+	<div class="container mx-auto px-6">
+        <BlurFade>
+		<h1 class="mb-12 text-center text-3xl sm:text-4xl lg:text-5xl xl:text-6xl
+ text-white font-bold">Blog</h1>
+        </BlurFade>
+ <div class="-mx-4 flex flex-wrap">
+			{#each blogPosts as post}
+            
+				<BlogCard title={post.title} content={post.content} imageUrl={post.imageUrl} />
+			{/each}
+		</div>
+	</div>
+</section>
--- src/lib/components/BlogCard.svelte
+++ src/lib/components/BlogCard.svelte
@@ -17,14 +17,16 @@
 	on:keydown={(e) => e.key === 'Enter' && toggleExpand()}
 	aria-expanded={isExpanded}
 >
-	<div class="overflow-hidden rounded-lg bg-white shadow-lg">
+	<div class="group overflow-hidden rounded-lg bg-slate-800/35 border border-slate-600 hover:bg-white/95 shadow-lg transition-all duration-300">
 		<img src={imageUrl} alt={title} class="h-48 w-full object-cover" />
 		<div class="p-6">
-			<h3 class="mb-2 text-xl font-bold">{title}</h3>
+			<h3 class="mb-2 text-gray-50 group-hover:text-gray-800 text-xl sm:text-2xl lg:text-3xl xl:text-3xl
+ font-bold transition-all duration-300">{title}</h3>
 			{#if isExpanded}
-				<p class="text-gray-700">{content}</p>
+				<p class="text-gray-200 group-hover:text-gray-700 text-base lg:text-lg
+  transition-all duration-300">{content}</p>
 			{:else}
-				<p class="text-gray-700">{content.substring(0, 100)}...</p>
+				<p class="text-gray-200 group-hover:text-gray-700 text-base lg:text-lg transition-all duration-300">{content.substring(0, 100)}...</p>
 			{/if}
 		</div>
 	</div>
