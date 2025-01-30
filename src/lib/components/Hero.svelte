1d663030f35b339477f0df02068c679a9aebb95a
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-01-31 00:48:05 +0530

    feat: hero

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import BlurFade from '$lib/components/ui/BlurFade.svelte';
+	import Globe from './ui/Globe.svelte';
 </script>
 
-<section class="bg-transparent pt-20 pb-10 text-center">
+<section class="bg-transparent pt-20 pb-28 lg:pb-0 text-center">
 	<div class="hero-container flex flex-col items-center px-6 md:px-12 lg:flex-row lg:px-36">
-		<div class="container mx-auto px-4 pt-10 text-center md:px-6 lg:text-right">
+		<div class="container mx-auto px-4  md:pt-20 lg:pt-32 pb-32 text-center md:px-6 lg:text-right">
 			<BlurFade delay={0.25}>
 				<h1
 					class="mb-4 text-4xl font-bold tracking-tighter text-white sm:text-5xl lg:text-6xl xl:text-7xl"
@@ -26,9 +27,10 @@
 				</a>
 			</BlurFade>
 		</div>
-		<div class="img hidden w-full max-w-xs sm:max-w-sm md:max-w-md lg:block lg:max-w-lg">
+		<div class="img w-full max-w-xs sm:max-w-sm md:max-w-md lg:block lg:max-w-lg">
 			<BlurFade delay={0.75}>
-			<img src="3delem.png" alt="elem-3d" class="h-auto w-full" />
+			<!-- <img src="3delem.png" alt="elem-3d" class="h-auto w-full" /> -->
+			 <Globe class="-top-32"/>
 			</BlurFade>
 		</div>
 	</div>
