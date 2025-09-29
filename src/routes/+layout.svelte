cc0ead33a64a1ca9e15a85b6a176d8e4e90e56ea
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-09-29 22:27:00 +0530

    add: Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Loading from '$lib/components/ui/Loading.svelte';
 	import { onMount } from 'svelte';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let isLoading: boolean = true;
 
